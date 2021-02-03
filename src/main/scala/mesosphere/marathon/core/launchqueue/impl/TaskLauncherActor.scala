1fc0852c625759666a163f6fde97f177d161f10b
Thomas Langé <t.lange@criteo.com>
2021-02-03 17:49:02 +0100

    Fix log presentation
    Putting back-off delay at the end makes it unlikely to see it because
    when there are too many scheduledInstances, log get truncated by
    rsyslog. Delay is more important and having an extract of few
    scheduledInstances is enough.

--- src/main/scala/mesosphere/marathon/core/launchqueue/impl/TaskLauncherActor.scala
+++ src/main/scala/mesosphere/marathon/core/launchqueue/impl/TaskLauncherActor.scala
@@ -346,7 +346,7 @@ private class TaskLauncherActor(
   private[this] def launchAllowed(now: Timestamp, configRef: RunSpecConfigRef): Boolean = launchAllowedAt.get(configRef).exists(_ <= now)
   private[this] def shouldLaunchInstances(now: Timestamp): Boolean = {
     if (scheduledInstances.nonEmpty || launchAllowedAt.nonEmpty)
-      logger.info(s"Found scheduled instances: ${scheduledInstances.map(_.instanceId).mkString(",")} and current back-off map: $launchAllowedAt")
+      logger.info(s"Found scheduled instances (current back-off map $launchAllowedAt): ${scheduledInstances.map(_.instanceId).mkString(",")}")
     scheduledInstances.nonEmpty && scheduledVersions.exists { configRef => launchAllowed(now, configRef) }
   }
 
